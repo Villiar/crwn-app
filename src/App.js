eb07c5cb469e9b14ce93199c5c191cf76b37e577
Villiar <alexkulyk87@gmail.com>
2020-04-30 19:30:31 +0300

    adding custom button and styles for if also adding SignIn component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/Shop';
 import HomePage from './pages/homepage/HomePage';
 import Header from './components/header/Header';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/SignInAndSignUp';
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact={true} path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUp} />
       </Switch>
     </div>
   );
