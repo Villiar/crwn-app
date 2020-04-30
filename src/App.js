72d40380cc2583760a03469c617bf61546e06300
Villiar <alexkulyk87@gmail.com>
2020-04-30 11:09:46 +0300

    adding Home page component and styling for it.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/Shop';
 import HomePage from './pages/homepage/HomePage';
+import Header from './components/header/Header';
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact={true} path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
